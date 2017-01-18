31057a75c0d1ca238c3254d6592600c06a2e1101
Christian Lagger <Christian Lagger>
2017-01-18 15:09:29 +0100

    removed security

--- src/main/java/at/fh/ima/swengs/rentacar/RentACarBackendApplication.java
+++ src/main/java/at/fh/ima/swengs/rentacar/RentACarBackendApplication.java
@@ -10,11 +10,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 public class RentACarBackendApplication {
